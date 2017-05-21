351358c33c6e52c86e5631826214b8014bdd8b55
Vasilij Schneidermann <mail@vasilij.de>
2017-05-21 20:45:07 +0200

    Replace strlen by snprintf, too

--- sledge/alloc.c
+++ sledge/alloc.c
@@ -426,20 +426,18 @@ Cell* alloc_string_from_bytes(Cell* bytes) {
 
 Cell* alloc_concat(Cell* str1, Cell* str2) {
   Cell* cell;
-  int size1, size2, newsize;
+  int size;
 
   if (!str1 || !str2) return alloc_string_copy("");
   if (str1->tag!=TAG_BYTES && str1->tag!=TAG_STR) return alloc_string_copy("");
   if (str2->tag!=TAG_BYTES && str2->tag!=TAG_STR) return alloc_string_copy("");
 
   cell = cell_alloc();
-  size1 = strlen(str1->ar.addr);
-  size2 = strlen(str2->ar.addr);
-  newsize = size1+size2;
-  cell->ar.addr = bytes_alloc(newsize+1);
-  cell->dr.size = newsize;
+  size = snprintf(NULL, 0, "%s%s", str1->ar.addr, str2->ar.addr);
+  cell->ar.addr = bytes_alloc(size+1);
+  cell->dr.size = size;
   cell->tag = TAG_STR;
-  snprintf(cell->ar.addr, newsize+1, "%s%s", str1->ar.addr, str2->ar.addr);
+  snprintf(cell->ar.addr, size+1, "%s%s", str1->ar.addr, str2->ar.addr);
   return cell;
 }
 
