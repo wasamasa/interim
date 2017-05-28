da85d836166c48f5b93004c3e0fce1c74a2e0d46
Vasilij Schneidermann <mail@vasilij.de>
2017-05-28 17:33:57 +0200

    Move posixfs_open logic into posixfs_read

--- devices/posixfs.c
+++ devices/posixfs.c
@@ -8,19 +8,26 @@
 #include <fcntl.h>
 #include <dirent.h>
 
-Cell* _file_cell;
-
 Cell* posixfs_open(Cell* cpath) {
-  char* path;
-  _file_cell = alloc_nil();
+  return alloc_int(1);
+}
 
-  if (!cpath || cpath->tag!=TAG_STR) {
-    printf("[posixfs] open error: non-string path given\r\n");
-    return _file_cell;
+Cell* posixfs_read(Cell* stream_cell) {
+  if (!stream_cell || stream_cell->tag != TAG_STREAM) {
+    printf("[posixfs] read error: non-stream argument given\n");
+    return alloc_nil();
   }
 
-  path = cpath->ar.addr;
-  
+  Stream* stream = stream_cell->ar.addr;
+  Cell* cpath = stream->path;
+  if (!cpath || cpath->tag!=TAG_STR) {
+    printf("[posixfs] read error: non-string path in stream\n");
+    return alloc_nil();
+  }
+
+  char* path = cpath->ar.addr;
+  printf("[posixfs] reading from %s\n", path);
+
   if (!strncmp(path,"/sd/",4)) {
     char* filename = NULL;
 
@@ -30,63 +37,49 @@ Cell* posixfs_open(Cell* cpath) {
 
     if (!filename || !filename[0]) filename = ".";
 
-    //printf("filename: %s\r\n",filename);
-    
-    if (filename) {
-      struct stat src_stat;
-      DIR* dirp;
-      int f;
-      off_t len;
-      
-      if (stat(filename, &src_stat)) {
-        _file_cell = alloc_string_copy("<file not found>");
-        return _file_cell;
-      }
-      len = src_stat.st_size;
+    struct stat src_stat;
+    DIR* dirp;
+    int f;
+    off_t len;
 
-      if ((dirp = opendir(filename))) {
-        struct dirent *dp;
-        Cell* nl = alloc_string_copy("\n");
-        _file_cell = alloc_string_copy("");
-        
-        do {
-          if ((dp = readdir(dirp)) != NULL) {
-            printf("dp: |%s|\r\n",dp->d_name);
-            _file_cell = alloc_concat(_file_cell,alloc_concat(alloc_string_copy(dp->d_name),nl));
-          }
-        } while (dp != NULL);
-        return _file_cell;
-      }
-
-      f = open(filename, O_RDONLY);
-      if (f>-1) {
-        Cell* res;
-        
-        printf("[posixfs] trying to read file of len %ld...\r\n",len);
-        res = alloc_num_bytes(len);
-        read(f, res->ar.addr, len);
-        close(f);
-        // TODO: close?
-        _file_cell = res;
-        return res;
-      } else {
-        // TODO should return error
-        printf("[posixfs] could not open file :(\r\n");
-        _file_cell = alloc_string_copy("<error: couldn't open file.>"); // FIXME hack
-        return _file_cell;
-      }
-      _file_cell = alloc_string_copy("<error: file not found.>");
-      return _file_cell;
-    } else {
-      // TODO dir
+    if (stat(filename, &src_stat)) {
+      return alloc_string_copy("<file not found>");
     }
+    len = src_stat.st_size;
+
+    if ((dirp = opendir(filename))) {
+      struct dirent *dp;
+      Cell* nl = alloc_string_copy("\n");
+      Cell* file_list = alloc_string_copy("");
+
+      do {
+        if ((dp = readdir(dirp)) != NULL) {
+          printf("dp: |%s|\r\n",dp->d_name);
+          file_list = alloc_concat(file_list,alloc_concat(alloc_string_copy(dp->d_name),nl));
+        }
+      } while (dp != NULL);
+      return file_list;
+    }
+
+    f = open(filename, O_RDONLY);
+    if (f>-1) {
+      Cell* res;
+
+      printf("[posixfs] trying to read file of len %ld...\r\n",len);
+      res = alloc_num_bytes(len);
+      read(f, res->ar.addr, len);
+      close(f);
+      // TODO: close?
+      return res;
+    } else {
+      // TODO should return error
+      printf("[posixfs] could not open file :(\r\n");
+      return alloc_string_copy("<error: couldn't open file.>"); // FIXME hack
+    }
+    return alloc_string_copy("<error: file not found.>");
   }
 
-  return _file_cell;
-}
-
-Cell* posixfs_read(Cell* stream) {
-  return _file_cell;
+  return alloc_nil();
 }
 
 Cell* posixfs_write(Cell* stream_cell, Cell* arg) {
