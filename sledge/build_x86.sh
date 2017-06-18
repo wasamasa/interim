9c34fd638dd2eecd84ad7e2e9e05701b549da516
Vasilij Schneidermann <mail@vasilij.de>
2017-06-18 08:34:37 +0200

    Use -Wall and -Wextra for x64 and x86 builds

--- sledge/build_x86.sh
+++ sledge/build_x86.sh
@@ -1,3 +1,3 @@
 
-gcc -m32 -g -o sledge --std=gnu99 -I. sledge.c readline.c reader.c writer.c alloc.c strmap.c stream.c ../devices/posixfs.c ../devices/sdl2.c -lm -lreadline -DREADLINE -lSDL2 -DCPU_X86 -DDEV_SDL -DDEV_POSIXFS
+gcc -m32 -g -o sledge --std=gnu99 -Wall -Wextra -I. sledge.c readline.c reader.c writer.c alloc.c strmap.c stream.c ../devices/posixfs.c ../devices/sdl2.c -lm -lreadline -DREADLINE -lSDL2 -DCPU_X86 -DDEV_SDL -DDEV_POSIXFS
 
